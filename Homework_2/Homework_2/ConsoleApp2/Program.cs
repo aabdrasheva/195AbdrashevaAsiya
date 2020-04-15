705dd9341aba99ab9f6bb2f84facb849bbf9e618
asiyaabdr <asiyabuk007@gmail.com>
2020-04-15 15:46:03 +0300

    еще какие то комменты

---
+++ Homework_2/Homework_2/ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using System.Data;
+using System.IO;
+
+namespace ConsoleApp2
+{
+
+    // класс и его члены объявлены как public
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // стандартный конструктор без параметров
+        public Person()
+        { }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // объект для сериализации
+            Person person = new Person("Tom", 29);
+            Console.WriteLine("Объект создан");
+
+            // передаем в конструктор тип класса
+            XmlSerializer formatter = new XmlSerializer(typeof(Person));
+
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("persons.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, person);
+
+                Console.WriteLine("Объект сериализован");
+            }
+
+            // десериализация
+            using (FileStream fs = new FileStream("persons.xml", FileMode.OpenOrCreate))
+            {
+                Person newPerson = (Person)formatter.Deserialize(fs);
+
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Имя: {newPerson.Name} --- Возраст: {newPerson.Age}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
